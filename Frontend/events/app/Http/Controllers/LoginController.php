ac40c27e2164c14c97a6c8f74526230c03b167cb
Chaïner KOMISSA ZOTSU <essikomissa@gmail.com>
2023-04-24 17:31:17 +0200

    Commit 3
    Création de controller

---
+++ Frontend/events/app/Http/Controllers/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+}
--- Frontend/events/routes/web.php
+++ Frontend/events/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('interface.dashboard');
+    return view('auth.login');
 });
 
 //Auth::routes();
