be4cd4156757d328afefcb9b8bedc55d9d590367
Chaïner KOMISSA ZOTSU <essikomissa@gmail.com>
2023-04-27 17:16:48 +0200

    Commit Auth2
    Redirections et page d'accueil

---
+++ Frontend/events/resources/views/interface/acceuil.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Event</title>
+</head>
+<body>
+    <h1>PAGE D'ACCEUIL</h1>
+    <a href="{{route('loginpage')}}">Mon compte</a>
+</body>
+</html>
